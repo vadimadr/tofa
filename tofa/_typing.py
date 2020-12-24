52adc0c8c85ff10be36e3cc66894ff54f55f26b3
Vadim Andronov <vadimadr@gmail.com>
2020-12-24 12:16:54 +0300

    Use Correct PIL module in _typing

--- tofa/_typing.py
+++ tofa/_typing.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 from typing import Iterable, Union
 
-import PIL
 import numpy as np
 import collections.abc
+from PIL.Image import Image
 
 array = np.ndarray
 path_like = Union[Path, str]
 array_like = Union[np.ndarray, Iterable, int, float]
-image_like = Union[np.ndarray, PIL.Image.Image]
+image_like = Union[np.ndarray, Image]
 
 
 Sequence = collections.abc.Sequence
